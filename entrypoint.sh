9a85393095ee1c763a06198f03aee01a0bb2c0a1
Aron <aronwc@gmail.com>
2025-09-29 21:05:12 -0500

    pin reqs; load .env

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
-
+set -a           # auto-export all variables
+source .env      # load .env file
+set +a
 # if /models is empty, seed it
 # if [ -z "$(ls -A /models/chroma_db)" ]; then
 echo "Seeding ChromaDB from remote archive…"
@@ -11,18 +13,7 @@ tar xzf /tmp/chroma_db.tar.gz -C $CHROMA_DB_DIR
 rm /tmp/chroma_db.tar.gz
 curl -fsSL $DATA_URL \
 -o $FLY_DATA/data.jsonl
-# fi
 
-# dropping ollama...
-# 1) Start Ollama in the background
-# echo "→ Starting Ollama server…"
-# ollama serve &
-
-# 2) Wait a moment for Ollama to bind
-# sleep 2
-
-# 3) Pull model for emebdding
-# ollama pull snowflake-arctic-embed2
 
 # 3) Launch Flask via Gunicorn (or flask run)
 echo "→ Starting Flask app…"
